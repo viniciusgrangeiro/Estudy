78f87a1bc3a98991cff0c63fc140bbe9cd2c622a
viniciusgrangeiro <vinicius.grangeiro@gmail.com>
2024-12-10 16:24:46 -0300

    adicionei alguns arquivos que contem dados sobre os livros e exibi esses dados na aplicacao

---
+++ mu-codes/book-analyzer.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+df_reviews = pd.read_csv("../datasets/customer reviews.csv")
+df_top100_books = pd.read_csv("../datasets/Top-100 Trending Books.csv")
+
+df_reviews
