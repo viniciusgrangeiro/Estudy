c42bce653a3237b4f2fcc67b351afdf37210cb0d
viniciusgrangeiro <vinicius,grangeiro@gmai.com>
2024-12-10 17:40:57 -0300

    adicionei uma barra de valores para filtrar os valores dos livros e exibi-los

--- mu-codes/book-analyzer.py
+++ mu-codes/book-analyzer.py
@@ -3,7 +3,18 @@ import pandas as pd
 
 st.set_page_config(layout="wide")
 
+# le os arquivos csv e armazenam em variaveis
 df_reviews = pd.read_csv("../datasets/customer reviews.csv")
 df_top100_books = pd.read_csv("../datasets/Top-100 Trending Books.csv")
 
-df_reviews
+# definindo quais sao so maiores e os menores valores de livtos
+price_max = df_top100_books["book price"].max()
+price_min = df_top100_books["book price"].min()
+
+# barra de pesquisa de valor maximo dos livros
+max_price = st.sidebar.slider("Price Range", price_min, price_max, price_max)
+
+# exibe a tabela de livros, mas somente os livros com valores <= que o valor maximo preferido
+df_books = df_top100_books[df_top100_books["book price"] <= max_price]
+
+df_books
