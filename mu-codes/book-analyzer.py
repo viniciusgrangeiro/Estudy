8575df9475f4019bc642ad5150449feb714de89e
viniciusgrangeiro <vinicius,grangeiro@gmai.com>
2024-12-10 18:19:22 -0300

    criei dois graficos que se baseiam no filtro de valor para exibir seus resultados e os alinhei lado a lado

--- mu-codes/book-analyzer.py
+++ mu-codes/book-analyzer.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 
@@ -16,5 +17,20 @@ max_price = st.sidebar.slider("Price Range", price_min, price_max, price_max)
 
 # exibe a tabela de livros, mas somente os livros com valores <= que o valor maximo preferido
 df_books = df_top100_books[df_top100_books["book price"] <= max_price]
-
 df_books
+
+# cria a figura de um grafico em barra contabilizando os livros a cada ano
+fig = px.bar(df_books["year of publication"].value_counts())
+# cria um histograma dos valores dos livros
+fig2 = px.histogram(df_books["book price"])
+
+# exibe as figuras um abaixo da outra
+# st.plotly_chart(fig)
+# st.plotly_chart(fig2)
+
+# exibe as figuras uma do lado do outra
+col1, col2 = st.columns(2)
+
+col1.plotly_chart(fig)
+col2.plotly_chart(fig2)
+
